008ed2bcabf81bed816748b20f746731c0702e4b
AdItYa057 <adityavardhan1995@gmail.com>
2022-11-21 11:47:52 +0530

    Version1

--- pythoon-rand.py
+++ pythoon-rand.py
@@ -4,7 +4,11 @@ a = random.randint(1, 10000)
 b = random.randint(1, 10000)
 c = a+b
 
-with open('.\Pyhton_app\Result.txt', 'w') as result:
+with open('./Pyhton_App_DevOps/Result.txt', 'a+') as result:
+    result.seek(0)
+    data = result.read(100)
+    if len(data) > 0:
+        result.write("\n")
     result.write("Sum of {A} + {B} is = {C}".format(A=a, B=b, C=c))
 
 print("Sum of {A} + {B} is = {C}".format(A=a, B=b, C=c))
